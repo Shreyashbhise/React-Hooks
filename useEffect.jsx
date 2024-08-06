4d20b2c53234ae5c57dfa349ca096ed5006ae437
Shreyash Bhise <108046802+Shreyashbhise@users.noreply.github.com>
2024-08-06 13:22:04 -0700

    Create useEffect.jsx

---
+++ useEffect.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+const MyComponent = () => { 
+    const [count,setcounter] = useState(0);
+    const [data,setData] = useState("Ram")
+
+    useEffect(()=> {
+        console.log("Component mounted")
+    },[data])
+
+    function updateCount() {
+        setcounter(count + 1)
+    } 
+
+    function  updateData() {
+        setData("ZSeeta")
+    }
+
+    
+    return (
+        <div>
+            <h1>Button clicked {count} times</h1>
+            <button onClick={updateCount}>Click</button>
+            <button onClick={updateData}>Update Data</button>
+        </div>
+    )
+}
+
+export default MyComponent;
+
+
+// css code
